b09fc8e65233a1b7efd4762b82342d936797e54c
azzeddine ouchen <153839792+03ouchen@users.noreply.github.com>
2024-01-17 14:17:56 +0100

    Add files via upload

---
+++ Voice Bot/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Voice_Bot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 31);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 91);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(118, 31);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 91);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Input";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(118, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Output";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "State : Awake";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(232, 152);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Voice Bot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Voice Bot/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis; // go to References clique left and add a system.speech
+using System.Speech.Recognition;
+using System.Diagnostics;
+
+namespace Voice_Bot
+{
+    public partial class Form1 : Form
+    {
+        SpeechSynthesizer s = new SpeechSynthesizer();
+        Boolean wake = true;
+        Choices list = new Choices();
+        public Form1()
+        {
+            SpeechRecognitionEngine rec = new SpeechRecognitionEngine();
+            list.Add(new String[] { "Hello","how are you","what time is it ?" , "what is Today?", "open bing","wake","sleep","restart","update"});
+            Grammar gr = new Grammar(new GrammarBuilder(list));
+
+            try
+            {
+                rec.RequestRecognizerUpdate();
+                rec.LoadGrammar(gr);
+                rec.SpeechHypothesized +=rec_SpeechHypothesized;
+                rec.SetInputToDefaultAudioDevice();
+                rec.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch { return; }
+
+            s.SelectVoiceByHints(VoiceGender.Female);
+            //s.Speak("hello,my name is robot");
+            InitializeComponent();
+        }
+        public void restart()
+        {
+            Process.Start(@"D:\Nouveau dossier\vs_professional"); // l'application de vs_professional va etre démarer 
+            Environment.Exit(0);
+        }
+        public void say(String h)
+        {
+            s.Speak(h);
+            textBox2.AppendText(h + "\n");
+        }
+
+        private void rec_SpeechHypothesized(object sender, SpeechHypothesizedEventArgs e)
+        {
+            String r = e.Result.Text;
+            if (r == "wake")
+            {
+                wake = true;
+                label3.Text = "wake state ";
+            }
+            if (r == "sleep") { 
+                wake = false;
+                label3.Text = "wake sleep mode";
+            }
+
+            if (wake == true) {
+
+                if (r == "restart" || r == "update")
+                {
+                    restart();
+                }
+
+
+            if(r == "hello")// what you say 
+            {
+                say("Hi");// what it says 
+
+                
+            }
+                
+            if (r == "what time is it ?")
+            {
+                say(DateTime.Now.ToString("h:mm tt"));
+            }
+
+            if (r == "what is today ?")
+            {
+                say(DateTime.Now.ToString("M/d/yyyy"));
+            }
+
+            if (r == "how are you ?")// what you say 
+            {
+                say("I'm fine, what about you");// what it says 
+
+            }
+                 
+            if (r == "open bing")
+            {
+                Process.Start("http://bing.com");
+            }
+            textBox2.AppendText(r + "\n");
+            } 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
